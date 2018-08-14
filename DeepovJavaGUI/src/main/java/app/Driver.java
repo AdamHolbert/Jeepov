9bcca912dfc9001dbc792cb6b285e02db23304ce
Jordon <jordon.bowden@gmail.com>
2018-08-14 16:30:18 -0600

    Changed a lot. Thanks.

--- DeepovJavaGUI/src/main/java/app/Driver.java
+++ DeepovJavaGUI/src/main/java/app/Driver.java
@@ -1,28 +1,12 @@
 package app;
-import javafx.application.*;
-import javafx.stage.Stage;
-import pieces.GridPiece;
-import javafx.scene.control.*;
-import javafx.scene.*;
-import javafx.scene.layout.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import java.io.FileInputStream;
 
-import com.gousslegend.deepov.Color;
 import com.gousslegend.deepov.Game;
 
-public class Driver extends Application{
-
-
+public class Driver {	
 	public static void main(String[] args) {
-		Application.launch(args);
+		App app = new App(args);
+		Game g = new Game(app);
+		app.run(g);
+		g.play();
 	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-
-	}
-
 }
