ca97e7eea2029e88cf2acac08a04638cdaf3eecd
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-08-20 15:54:13 -0600

    is

--- DeepovJavaGUI/src/main/java/pieces/ChessBoard.java
+++ DeepovJavaGUI/src/main/java/pieces/ChessBoard.java
@@ -2,6 +2,8 @@ package pieces;
 
 import javafx.scene.control.*;
 
+import java.util.ArrayList;
+
 import com.gousslegend.deepov.Color;
 import com.gousslegend.deepov.Game;
 
@@ -14,12 +16,15 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class ChessBoard extends GridPane {
-	
+//	GridPiece[][] list = new GridPiece[8][8];
+	ArrayList<GridPiece> list = new ArrayList<>();
+	private Game game = null;
 	public ChessBoard(Game g) throws Exception{
-		displayChessBoard(g);
+		this.game = g;
+		addToGridPane(g);
 	}
 	
-	private void displayChessBoard(Game g) throws Exception{
+	private void addToGridPane(Game g) throws Exception{
 		int i = 0;
 		boolean flip = false;
 		for(int a = 0; a < 8; a++){
@@ -29,10 +34,13 @@ public class ChessBoard extends GridPane {
 				if((a - b) % 2 == 0) bgColor = Color.BLACK;
 				else bgColor = Color.WHITE;
 				
-				if(b == 2 || b == 3 || b == 4 || b == 5)
-					this.add(new GridPiece("", bgColor), a, b, 1, 1);
+				if(b == 2 || b == 3 || b == 4 || b == 5){
+					this.add(new GridPiece("", bgColor, this), a, b, 1, 1);
+//					list[a][b] = new GridPiece("", bgColor, this);
+					list.add(new GridPiece("", bgColor, this));
+				}
 				//The list of pieces on board
-				if(i < 32){
+				if(i < g.getBoard().getNumberOfPieces()){
 					//Background colors alternate through list
 					if(i % 2 == 0 && flip == false) bgColor = Color.BLACK;
 					else if(i % 2 == 1 && flip == true) bgColor = Color.BLACK;
@@ -42,37 +50,41 @@ public class ChessBoard extends GridPane {
 					int y = g.getBoard().getPieces().get(i).getPosition().getY();
 					String simpleName = g.getBoard().getPieces().get(i).getClass().getSimpleName();
 					Color color = g.getBoard().getPieces().get(i).getColor();
+					GridPiece tempPiece = null;
 					
 					if(simpleName.equals("Pawn") && color == Color.BLACK){
-						this.add(new GridPiece("bp", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("bp", bgColor, this);
 					}else if(simpleName.equals("Pawn") && color == Color.WHITE){
-						this.add(new GridPiece("wp", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wp", bgColor, this);
 					}
 					if(simpleName.equals("Rook") && color == Color.BLACK){
-						this.add(new GridPiece("br", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("br", bgColor, this);
 					}else if(simpleName.equals("Rook") && color == Color.WHITE){
-						this.add(new GridPiece("wr", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wr", bgColor, this);
 					}
 					if(simpleName.equals("Knight") && color == Color.BLACK){
-						this.add(new GridPiece("bn", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("bn", bgColor, this);
 					}else if(simpleName.equals("Knight") && color == Color.WHITE){
-						this.add(new GridPiece("wn", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wn", bgColor, this);
 					}
 					if(simpleName.equals("Bishop") && color == Color.BLACK){
-						this.add(new GridPiece("bb", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("bb", bgColor, this);
 					}else if(simpleName.equals("Bishop") && color == Color.WHITE){
-						this.add(new GridPiece("wb", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wb", bgColor, this);
 					}
 					if(simpleName.equals("Queen") && color == Color.BLACK){
-						this.add(new GridPiece("bq", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("bq", bgColor, this);
 					}else if(simpleName.equals("Queen") && color == Color.WHITE){
-						this.add(new GridPiece("wq", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wq", bgColor, this);
 					}
 					if(simpleName.equals("King") && color == Color.BLACK){
-						this.add(new GridPiece("bk", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("bk", bgColor, this);
 					}else if(simpleName.equals("King") && color == Color.WHITE){
-						this.add(new GridPiece("wk", bgColor), x, y, 1, 1);
+						tempPiece = new GridPiece("wk", bgColor, this);
 					}
+					this.add(tempPiece, x, y, 1, 1);
+//					list[x][y] = tempPiece;
+					list.add(tempPiece);
 					i++;
 				}
 				//List is from 0 - 31. Flip boolean on multiples of 4
@@ -80,6 +92,7 @@ public class ChessBoard extends GridPane {
 				else if(i % 8 == 0) flip = false; //i == 8 || i == 16 || i == 24
 			}
 		}
-
 	}
+
+
 }
