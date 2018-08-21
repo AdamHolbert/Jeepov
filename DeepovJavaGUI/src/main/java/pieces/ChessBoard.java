d7726a07b6dd83a9a547ba9fb270b150952cf974
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-08-20 21:46:10 -0600

    We can move pieces

--- DeepovJavaGUI/src/main/java/pieces/ChessBoard.java
+++ DeepovJavaGUI/src/main/java/pieces/ChessBoard.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 
 import com.gousslegend.deepov.Color;
 import com.gousslegend.deepov.Game;
+import com.gousslegend.deepov.Move;
+import com.gousslegend.deepov.MoveList;
+import com.gousslegend.deepov.Position;
+import com.gousslegend.deepov.pieces.Piece;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,9 +20,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class ChessBoard extends GridPane {
-//	GridPiece[][] list = new GridPiece[8][8];
-	ArrayList<GridPiece> list = new ArrayList<>();
+	GridPiece[][] list = new GridPiece[8][8];
+//	ArrayList<GridPiece> list = new ArrayList<>();
 	private Game game = null;
+	private GridPiece selectedGridPiece = null;
 	public ChessBoard(Game g) throws Exception{
 		this.game = g;
 		addToGridPane(g);
@@ -30,61 +35,61 @@ public class ChessBoard extends GridPane {
 		for(int a = 0; a < 8; a++){
 			for(int b = 0; b < 8; b++){
 				Color bgColor = null;
+				GridPiece tempPiece = null;
 				//Empty squares between teams' pieces
 				if((a - b) % 2 == 0) bgColor = Color.BLACK;
 				else bgColor = Color.WHITE;
 				
 				if(b == 2 || b == 3 || b == 4 || b == 5){
-					this.add(new GridPiece("", bgColor, this), a, b, 1, 1);
-//					list[a][b] = new GridPiece("", bgColor, this);
-					list.add(new GridPiece("", bgColor, this));
+					tempPiece = new GridPiece("", bgColor, this, null);
+					this.add(tempPiece, a, b, 1, 1);
+					list[a][b] = tempPiece;
 				}
 				//The list of pieces on board
-				if(i < g.getBoard().getNumberOfPieces()){
+				if(i < g.getBoardPieces().size()){
 					//Background colors alternate through list
 					if(i % 2 == 0 && flip == false) bgColor = Color.BLACK;
 					else if(i % 2 == 1 && flip == true) bgColor = Color.BLACK;
 					else bgColor = Color.WHITE;
 					
-					int x = g.getBoard().getPieces().get(i).getPosition().getX();
-					int y = g.getBoard().getPieces().get(i).getPosition().getY();
-					String simpleName = g.getBoard().getPieces().get(i).getClass().getSimpleName();
-					Color color = g.getBoard().getPieces().get(i).getColor();
-					GridPiece tempPiece = null;
+					Piece currentPiece = g.getBoardPieces().get(i);
+					int x = currentPiece.getPosition().getX();
+					int y = currentPiece.getPosition().getY();
+					String simpleName =  currentPiece.getClass().getSimpleName();
+					Color color = currentPiece.getColor();
 					
 					if(simpleName.equals("Pawn") && color == Color.BLACK){
-						tempPiece = new GridPiece("bp", bgColor, this);
+						tempPiece = new GridPiece("bp", bgColor, this, currentPiece);
 					}else if(simpleName.equals("Pawn") && color == Color.WHITE){
-						tempPiece = new GridPiece("wp", bgColor, this);
+						tempPiece = new GridPiece("wp", bgColor, this, currentPiece);
 					}
 					if(simpleName.equals("Rook") && color == Color.BLACK){
-						tempPiece = new GridPiece("br", bgColor, this);
+						tempPiece = new GridPiece("br", bgColor, this, currentPiece);
 					}else if(simpleName.equals("Rook") && color == Color.WHITE){
-						tempPiece = new GridPiece("wr", bgColor, this);
+						tempPiece = new GridPiece("wr", bgColor, this, currentPiece);
 					}
 					if(simpleName.equals("Knight") && color == Color.BLACK){
-						tempPiece = new GridPiece("bn", bgColor, this);
+						tempPiece = new GridPiece("bn", bgColor, this, currentPiece);
 					}else if(simpleName.equals("Knight") && color == Color.WHITE){
-						tempPiece = new GridPiece("wn", bgColor, this);
+						tempPiece = new GridPiece("wn", bgColor, this, currentPiece);
 					}
 					if(simpleName.equals("Bishop") && color == Color.BLACK){
-						tempPiece = new GridPiece("bb", bgColor, this);
+						tempPiece = new GridPiece("bb", bgColor, this, currentPiece);
 					}else if(simpleName.equals("Bishop") && color == Color.WHITE){
-						tempPiece = new GridPiece("wb", bgColor, this);
+						tempPiece = new GridPiece("wb", bgColor, this, currentPiece);
 					}
 					if(simpleName.equals("Queen") && color == Color.BLACK){
-						tempPiece = new GridPiece("bq", bgColor, this);
+						tempPiece = new GridPiece("bq", bgColor, this, currentPiece);
 					}else if(simpleName.equals("Queen") && color == Color.WHITE){
-						tempPiece = new GridPiece("wq", bgColor, this);
+						tempPiece = new GridPiece("wq", bgColor, this, currentPiece);
 					}
 					if(simpleName.equals("King") && color == Color.BLACK){
-						tempPiece = new GridPiece("bk", bgColor, this);
+						tempPiece = new GridPiece("bk", bgColor, this, currentPiece);
 					}else if(simpleName.equals("King") && color == Color.WHITE){
-						tempPiece = new GridPiece("wk", bgColor, this);
+						tempPiece = new GridPiece("wk", bgColor, this, currentPiece);
 					}
 					this.add(tempPiece, x, y, 1, 1);
-//					list[x][y] = tempPiece;
-					list.add(tempPiece);
+					list[x][y] = tempPiece;
 					i++;
 				}
 				//List is from 0 - 31. Flip boolean on multiples of 4
@@ -94,5 +99,57 @@ public class ChessBoard extends GridPane {
 		}
 	}
 
+	public void selectedPiece(GridPiece gridPiece){
+		boolean validSelection = false;
+		for(Piece piece : game.getSelectable()) {
+			if(piece == gridPiece.getPiece()){
+				validSelection = true;
+				selectedGridPiece = gridPiece;
+			}
+		}
+		if(validSelection){
+			//deselect any selected pieces
+			for(int i = 0; i < list.length; i++){
+				for(int j = 0; j < list[i].length; j++){
+					list[i][j].unhighlight();
+				}
+			}
+			//highlight possible move areas
+			selectedGridPiece.highlight();
+			MoveList ml = gridPiece.getPiece().getLegalMoves();
+			for(int i = 0; i < ml.size(); i++){
+				Move move = ml.getList().get(i);
+				int x = move.getDestination().getX();
+				int y = move.getDestination().getY();
+				list[x][y].highlight();
+				list[x][y].pos.setX((byte)x);
+				list[x][y].pos.setY((byte)y);
+			}
+		}
+	}
+	
+	public void makeMove(String newColor, Position position) throws Exception{
+		//Set previous position to null
+		int oldX = selectedGridPiece.getPiece().getPosition().getX();
+		int oldY = selectedGridPiece.getPiece().getPosition().getY();
+		Color bgColor = selectedGridPiece.getColor();
+		list[oldX][oldY] = new GridPiece("", bgColor, this, null);
+		//Move selected Piece to new position
+		int newX = position.getX();
+		int newY = position.getY();
+		list[newX][newY] = selectedGridPiece;
+		list[newX][newY].setColor(newColor);
+		
+		update();
+	}
 
+	public void update(){
+		this.getChildren().clear();
+		for(int i = 0; i < list.length; i++){
+			for(int j = 0; j < list[i].length; j++){
+				list[i][j].unhighlight();
+				this.add(list[i][j], i, j, 1, 1);
+			}
+		}
+	}
 }
--- DeepovJavaGUI/src/main/java/pieces/GridPiece.java
+++ DeepovJavaGUI/src/main/java/pieces/GridPiece.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import com.gousslegend.deepov.Color;
+import com.gousslegend.deepov.Position;
+import com.gousslegend.deepov.pieces.Piece;
 import com.gousslegend.deepov.pieces.Piece.ChessPieceType;
 
 import javafx.event.ActionEvent;
@@ -18,10 +20,15 @@ import javafx.scene.input.MouseEvent;
 public class GridPiece extends Button{
 	public String color;
 	private String name;
+	public ChessBoard board;
+	private Piece piece;
+	public Position pos = new Position();
 	
 	@SuppressWarnings("restriction")
-	public GridPiece(String fileName, Color bgColor, ChessBoard board) throws Exception{
+	public GridPiece(String fileName, Color bgColor, ChessBoard board, Piece currentPiece) throws Exception{
 		this.name = fileName;
+		this.board = board;
+		this.piece = currentPiece;
 		if(!fileName.equalsIgnoreCase("")){
 			ImageView imageView = new ImageView(new Image(new FileInputStream("src/main/resources/"+fileName+".png")));
 			imageView.setFitWidth(30);
@@ -34,21 +41,27 @@ public class GridPiece extends Button{
 			this.setMaxSize(50, 50);
 		}
 		if(bgColor == Color.BLACK){
-			this.color = "#000000";
+			this.color = "#000";
 		}else if(bgColor == Color.WHITE){
-			this.color = "#ffffff";
+			this.color = "#fff";
 		}
 		this.setStyle("-fx-background-color: "+this.color+"; -fx-border-width: 0px;");
-		//Selected
-		this.setOnAction((value) -> {				
-			
+		//On Click
+		this.setOnAction((value) -> {
+			if(!this.getStyle().equals("-fx-background-color: #33f;") || !name.equals(""))
+				board.selectedPiece(this);
+			else {
+				try { board.makeMove(this.color, pos);
+				} catch (Exception e) { e.printStackTrace(); }
+//				this.setStyle("-fx-background-color: #f33;");
+			}
 		});
 	}
 
-	public void selected() {
-		this.setStyle("-fx-border-color: #00ff00; -fx-background-color: "+this.color+";");
+	public void highlight() {
+		this.setStyle("-fx-background-color: #33f;");
 	}
-	public void unselected() {
+	public void unhighlight() {
 		this.setStyle("-fx-background-color: "+this.color+";");
 	}
 
@@ -69,5 +82,15 @@ public class GridPiece extends Button{
 		else
 			type = null;
 		return type;
+	}
+
+	public Piece getPiece() {
+		return piece;
+	}
+	public Color getColor(){
+		return (this.color.equals("#000")) ? Color.BLACK : Color.WHITE;
+	}
+	public void setColor(String bgColor){
+		this.color = bgColor;
 	}
 }
